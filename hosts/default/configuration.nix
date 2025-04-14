1ed10cddf48687cca084bcfd5e9e8e75b9998bc0
Hitalo Siqueira <hsiqueira10@gmail.com>
2025-04-14 23:22:14 +0100

    audio setup

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -48,6 +48,17 @@
     };
   };
 
+  hardware.enableAllFirmware = true;
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    jack.enable = true;
+  };
+
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
