471f6dacc2346cf06889d03dc2cb7633ea788844
Hitalo Siqueira <hsiqueira10@gmail.com>
2026-07-04 15:34:57 +0100

    add home manager

--- flake.nix
+++ flake.nix
@@ -3,12 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, home-manager, ...}@inputs: {
     nixosConfigurations.hsiq = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
       system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+      modules = [ 
+        ./configuration.nix
+        home-manager.nixosModules.default
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.hsiq = ./home.nix; 
+            };
+          }
+      ];
     };
   };
 }
