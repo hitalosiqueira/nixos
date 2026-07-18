7061c41323a20316fe70c0d626f4bcdf2bd9bec3
Hitalo Siqueira <hsiqueira10@gmail.com>
2026-07-18 18:09:48 +0100

    home modules now live under home-manager/ directory

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.hsiq = import ./home.nix;
+            home-manager.users.hsiq = import ./home-manager;
             home-manager.extraSpecialArgs = { inherit inputs; };
           }
         ];
